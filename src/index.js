62c1616576bbfe766e16f6d4b3459cd1519941ca
jemish kevadiya <jemishkevadiya007@gmail.com>
2025-02-13 16:58:28 -0500

    error fixed

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import StripeProvider from './StripeProvider'; // Import StripeProvider
+// import StripeProvider from './StripeProvider'; // Import StripeProvider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     {/* Wrap the App with StripeProvider to provide Stripe context */}
-    <StripeProvider>
+    {/* <StripeProvider> */}
       <App />
-    </StripeProvider>
+    {/* </StripeProvider> */}
   </React.StrictMode>
 );
 
